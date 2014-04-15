00b11fcd1d09858a2ae3ad4f78fbcba37ae345f3
Hassan Yasir <hassan_yasi@hotmail.com>
2014-04-15 13:35:34 +0300

    Harjottelumateriaalia

---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public int hhh;
+	public string nimi;
+
+	// Use this for initialization
+	void Start () {
+		NewBehaviourScript g = new NewBehaviourScript ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
